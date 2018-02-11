1c380b64500a191e40c3a6c67bac0bdba6953838
Mattia Lorenzutti <mattia.lorenzutti@gmail.com>
2018-02-11 16:29:36 +0100

    Move all source file to /src, new dev script, SCSS, HTML template, bundle now goes to /dist, removed node_modules from the repository

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+})
 
 module.exports = {
-    entry: './index.js',
+    entry: __dirname + '/src/index.js',
     output: {
         filename: '[name].bundle.js',
-        path: __dirname,
-        publicPath: __dirname
+        path: __dirname + '/dist'
     },
     devtool: 'source-map',
-    watch: true,
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
     plugins: [
-        // new UglifyJsPlugin()
+        extractSass,
+        new CopyWebpackPlugin([
+            { from: 'src/static' }
+        ]),
+        new HtmlWebpackPlugin({
+            template : __dirname + '/src/template.html'
+        })
     ]
 };
