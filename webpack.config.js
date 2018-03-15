c10612d06f354f13dd7657858198e1c691db8098
Mattia Lorenzutti <mattia.lorenzutti@gmail.com>
2018-03-15 15:26:01 +0100

    Another React Version

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,100 @@
+/**
+ * Development Webpack Configuration
+ */
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { resolve } = require('path');
 
+const webpack = require('webpack');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
-})
-
-
-let bs = null
-if (process.env.TYPE === "browsersync") {
-    bs = new BrowserSyncPlugin({
-        // browse to http://localhost:3000/ during development,
-        // ./public directory is being served
-        host: 'localhost',
-        port: 3000,
-        server: { baseDir: ['dist'] }
-    })
-}
-
-let uglify = null;
-if (process.env.NODE_ENV === "prod") {
-    uglify = new UglifyJsPlugin();
-}
+const NODE_HOST = 'localhost';
+const NODE_PORT = 8111;
 
 module.exports = {
-    entry: __dirname + '/src/index.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: __dirname + '/dist'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: extractSass.extract({
-                use: [{
-                    loader: "css-loader"
-                }, {
-                    loader: "sass-loader"
-                }],
-                // use style-loader in development
-                fallback: "style-loader"
-            })
-        }]
-    },
-    plugins: [
-        extractSass,
-        new CopyWebpackPlugin([
-            { from: 'src/static' }
-        ]),
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/template.html'
-        }),
-        bs,
-        uglify
-    ].filter(Boolean)
-};
+  
+  devtool: 'cheap-module-eval-source-map',
+  
+  context: resolve(__dirname, 'src'),
+  
+  entry: [
+    'react-hot-loader/patch',
+    `webpack-dev-server/client?http://${NODE_HOST}:${NODE_PORT}`,
+    './'
+  ],
+  
+  output: {
+    filename: 'app-[hash].js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]', 'postcss-loader', 'sass-loader'],
+        exclude: /node_modules/
+      },
+      { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=[name]-[hash].[ext]' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+  
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    alias: {
+      variables: resolve(__dirname, 'src/scss/utils/variables'),
+      mixins: resolve(__dirname, 'src/scss/utils/mixins'),
+      respond: resolve(__dirname, 'src/scss/utils/respond')
+    }
+  },
+  
+  devServer: {
+    host: NODE_HOST,
+    port: NODE_PORT,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/',
+    historyApiFallback: true,
+    hot: true,
+    noInfo: false,
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: true
+    }
+  },
+  
+  plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new DashboardPlugin()
+  ]
+  
+}
